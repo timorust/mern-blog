e9976c8163e653a62496bc8dc5e42057323b3ba5
Timor Rust <timorust1@gmail.com>
2022-07-01 23:21:22 +0300

    authorization

--- index.js
+++ index.js
@@ -16,6 +16,46 @@ mongoose
 const app = express();
 app.use(express.json());
 
+app.post("/auth/login", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Login or Password Invalid",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Authorization failed",
+    });
+  }
+});
+
 app.post("/auth/register", registerValidation, async (req, res) => {
   try {
     const errors = validationResult(req);
@@ -44,9 +84,9 @@ app.post("/auth/register", registerValidation, async (req, res) => {
       {
         expiresIn: "30d",
       }
-	  );
-	  
-	  const {passwordHash, ...userData} = user._doc
+    );
+
+    const { passwordHash, ...userData } = user._doc;
 
     res.json({ ...userData, token });
   } catch (error) {
