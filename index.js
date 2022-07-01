0f44d97a906532b1c02a3f90f1febc06fb198de1
Timor Rust <timorust1@gmail.com>
2022-07-01 22:52:18 +0300

    registration

--- index.js
+++ index.js
@@ -1,44 +1,65 @@
 import express from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import mongoose from 'mongoose'
+import { validationResult } from "express-validator";
+import { registerValidation } from "./validations/auth.js";
+import UserModel from "./models/User.js";
 
 mongoose
   .connect(
-    "mongodb+srv://mernblog:mern@mernblog.lt9r4.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://mernblog:mern@mernblog.lt9r4.mongodb.net/blog?retryWrites=true&w=majority"
   )
   .then(() => console.log("DB OK"))
-  .catch(err => console.log('DB error', err));
+  .catch(err => console.log("DB error", err));
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.use(express.json())
-app.get('/', (req, res) => {
-	res.send('Hello World')
-})
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
 
-app.post('/auth/login', (req, res) => {
-	console.log(req.body)
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-	if (req.body.email === 'test@test.com') {
-		const token = jwt.sign(
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
       {
-        email: req.body.email,
-        fullName: "Timor Rust",
+        _id: user._id,
       },
-      "secret123"
-    );
-	}
-	
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+	  );
+	  
+	  const {passwordHash, ...userData} = user._doc
 
-	res.json({
-		success: true,
-		token,
-	})
-})
+    res.json({ ...userData, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Registration failed",
+    });
+  }
+});
 
-app.listen(4444, (err) => {
-	if (err) {
-		console.log(err)
-	}
-	console.log('Server Ok')
-})
+app.listen(4444, err => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Server Ok");
+});
