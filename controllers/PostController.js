a9e46ad693fbd034bba86b51ecdcdcf30a7ea606
Timor Rust <timorust1@gmail.com>
2022-07-02 19:38:13 +0300

    PostModel

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,7 +1,21 @@
-export const create = (req, res) => {
-	try {
-		
-	} catch (error) {
-		
-	}
-}
+import PostModel from "../models/Post.js";
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Article not built",
+    });
+  }
+};
