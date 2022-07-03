ca4b462579ab4db64226102db023f0fcd55f077f
Timor Rust <timorust1@gmail.com>
2022-07-03 18:58:05 +0300

    getOne

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,55 @@
 import PostModel from "../models/Post.js";
 
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate("user").exec();
+
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Unable to get articles",
+    });
+  }
+};
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(error);
+          return res.status(500).json({
+            message: "Articles cannot be returned",
+          });
+        }
+
+        if (!doc) {
+          res.status(404).json({
+            message: "Article not found",
+          });
+        }
+
+        res.json(doc);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Unable to get articles",
+    });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.post("/auth/login", loginValidation, UserController.login);
 app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
 
-//app.get("/posts", PostController.getAll);
-//app.get("/posts/:id", PostController.getOne);
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, PostController.create);
 //app.delete("/posts/:id", PostController.remove);
 //app.patch("/posts/:id", PostController.update);
