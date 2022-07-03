62c566c34dba486642e321e96201e03eaa29f63b
Timor Rust <timorust1@gmail.com>
2022-07-03 21:01:49 +0300

    CRUD

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -104,3 +104,30 @@ export const create = async (req, res) => {
     });
   }
 };
+
+export const update = async (req, res) => {
+	try {
+		const postId = req.params.id
+
+		await PostModel.updateOne(
+			{
+				_id: postId,
+			},
+			{
+				title: req.body.title,
+				text: req.body.text,
+				imageUrl: req.body.imageUrl,
+				user: req.userId,
+				tags: req.body.tags,
+			}
+		)
+		res.json({
+			success: true,
+		})
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({
+			message: 'Update wrong'
+		})
+	}
+}
--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.get("/posts", PostController.getAll);
-app.get("/posts/:id", PostController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, PostController.create);
 app.delete("/posts/:id", checkAuth, PostController.remove);
-//app.patch("/posts/:id", PostController.update);
+app.get("/posts/:id", PostController.getOne);
+app.patch("/posts/:id", checkAuth, PostController.update);
 
 app.listen(4444, err => {
   if (err) {
