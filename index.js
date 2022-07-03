9c6aa3b3528c7fd26388fb987254514d9bdd726a
Timor Rust <timorust1@gmail.com>
2022-07-03 17:35:24 +0300

    post-create

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import express from "express";
 
 import mongoose from "mongoose";
 
-import { registerValidation, loginValidation } from "./validations.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 
 import checkAuth from "./utils/checkAuth.js";
 
 import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 
 mongoose
   .connect(
@@ -19,10 +24,14 @@ const app = express();
 app.use(express.json());
 
 app.post("/auth/login", loginValidation, UserController.login);
-
 app.post("/auth/register", registerValidation, UserController.register);
-
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+//app.get("/posts", PostController.getAll);
+//app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+//app.delete("/posts/:id", PostController.remove);
+//app.patch("/posts/:id", PostController.update);
 
 app.listen(4444, err => {
   if (err) {
